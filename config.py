aba78fd23fe9ddcb2f8b4e8660c4a81dd9d1fd05
Vickyvkp95l <panditvicky875@gmail.com>
2025-02-06 01:05:35 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+# SCRIPT BY: Riyahacksyt  
+
+# Enter your Bot Token (Get it from @BotFather)
+BOT_TOKEN = "7925081549:AAFbOatPlgt2mZhqLCTnNoYumE0ZlK6reHw"
+
+# Enter your Telegram username (without @)
+OWNER_USERNAME = "VKP_950"
+
+# Enter your admin ID (Get it from @MissRose_bot by typing /info)
+ADMIN_IDS = ["6127576279"]  # Keep it as a list for multiple admins
+
+# Enter your Telegram channel link
+CHANNEL_LINK = "https://t.me/+Y64pMbJhPgoxNmU1"  # Replace with your actual channel link
+
+# Enter the filename of your channel logo (Image must be in the same folder as the script)
+CHANNEL_LOGO = "channel_logo.png"  # Replace with actual image file name
